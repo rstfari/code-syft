0afce26f2dac83d7b39535f7710eff904f6d13c5
rstfari <144150326+rstfari@users.noreply.github.com>
2024-01-05 01:48:29 -0500

    added rounding to searchbar

--- app/components/search/searchbar/index.tsx
+++ app/components/search/searchbar/index.tsx
@@ -1,13 +1,19 @@
 import React from "react";
+import Image from "next/image";
 
 const Searchbar = () => {
   return (
+    <div className="flex h-9 mt-3 w-full bg-slate-400 rounded-full px-3 hover:shadow-lg focus-within:shadow-lg items-center" >
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7 mr-3">
+  <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+</svg>
+
     <input
-      className=" h-9 w-full mt-2 flex-grow bg-slate-400
-       outline-none p-2 rounded-full  hover:shadow-lg"
+      className="flex-grow outline-none bg-slate-400"
       type="search"
       id="searchbar"
     />
+    </div>
   );
 };
 
