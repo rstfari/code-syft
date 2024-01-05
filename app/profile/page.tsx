87300af5b798a326768332b4f43849b54d925c37
rstfari <144150326+rstfari@users.noreply.github.com>
2024-01-05 02:50:55 -0500

    basic profile page layout

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Logo from "../components/search/logo";
+import Link from "next/link";
+import Navigation from "../components/navigation";
+import Foot from "../components/footer";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div className="flex flex-col h-screen items-center">
+      <Navigation />
+      <Link href="/">
+        <Logo headText="Profile"/>
+      </Link>
+      <Foot />
+    </div>
+  )
 };
 
 export default Profile;
