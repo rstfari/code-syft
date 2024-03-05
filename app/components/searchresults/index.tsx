b543f0f1e2e2d9822f235e2cf544bee9671549ee
christopher.tafari <christopher.tafari@gmail.com>
2024-03-05 18:38:33 +0000

    search results responsiveness

--- app/components/searchresults/index.tsx
+++ app/components/searchresults/index.tsx
@@ -3,13 +3,13 @@ import PaginationButtons from "../paginationbuttons";
 export default function SearchResults({results}) {
     
   return (
-    <div>
-        <p className="text-white pl-10">
+    <div className="w-full">
+        <p className="text-white text-center">
           Results: {results.searchInformation.formattedTotalResults} in {results.searchInformation.formattedSearchTime} seconds.
         </p>
 
         {results.items?.map((result) => (
-          <div key={result.link} className="text-white mb-8">
+          <div key={result.link} className="text-white mb-4 w-11/12 mx-auto text-xs bg-slate-600 bg-opacity-10 p-4 rounded-lg sm:w-9/12 md:w-9/12 sm:text-sm lg:w-7/12">
             <div className="group">
               <a href={result.link} className="text-gray-500 text-sml">
                 {result.formattedUrl}
